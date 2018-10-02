c998fc7c5ff64af115be05ca0f8442480ebb60f0
ostap <ostapstephan@gmail.com>
2018-10-02 00:43:53 -0400

    added more epochs

--- hw4curro/CIFAR10.py
+++ hw4curro/CIFAR10.py
@@ -36,14 +36,14 @@ def genTrainAndVal(f,l): #split the features and labels of the training data 80:
 # load cifar 10
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-# load cifar 100
-#from keras.datasets import cifar100
-#(x_train, y_train), (x_test, y_test) = cifar100.load_data(label_mode='fine')
+mean = np.mean(x_train, axis=(0,1,2,3))
+std = np.std(x_train, axis=(0,1,2,3))
+x_train = (x_train-mean)/(std+1e-7)
+x_test = (x_test-mean)/(std+1e-7)
 
-#fonvert to float and normalize
+#convert to float and normalize
 x_train,x_test = x_train.astype('float32'),x_test.astype('float32')
 x_train,x_test = x_train/255,x_test/255
-
 x_t,y_t,x_v,y_v =genTrainAndVal(x_train,y_train)
 
 #print Shapes
@@ -59,6 +59,39 @@ y_v = keras.utils.to_categorical(y_v,num_classes)
 y_test  = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+model.compile(loss=keras.losses.categorical_crossentropy ,
+	optimizer=keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False),
+	metrics=['accuracy'])
+
+model.fit(x_t, y_t,
+	batch_size=BATCH_SIZE,
+	epochs = epochs,
+	verbose = 1,
+	validation_data =(x_v,y_v))
+
+score = model.evaluate(x_test,y_test,verbose= 1)
+print("Test loss:",score[0])
+print("Test accuracy:",score[1])
+
+
+
+
+
+'''
 model.add(Conv2D(64,kernel_size=(5,5),
 	strides= 1,
 	padding='valid',
@@ -93,25 +126,8 @@ model.add(Conv2D(2048,kernel_size=(1,1),
 	kernel_initializer='glorot_uniform',
 	input_shape=x_t[0].shape))
 
-
-
 model.add(Flatten())
 model.add(Dense(256,activation='relu'))
 model.add(Dropout(DROP_RATE))
 model.add(Dense(num_classes,activation="softmax"))
-
-model.compile(loss=keras.losses.categorical_crossentropy ,
-	optimizer=keras.optimizers.Adam(lr=0.001),
-	metrics=['accuracy'])
-
-model.fit(x_t, y_t,
-	batch_size=BATCH_SIZE,
-	epochs = epochs,
-	verbose = 1,
-	validation_data =(x_v,y_v))
-
-score = model.evaluate(x_test,y_test,verbose= 1)
-print("Test loss:",score[0])
-print("Test accuracy:",score[1])
-
-
+'''
