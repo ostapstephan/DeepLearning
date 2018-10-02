0e0a93a9aebcad390a9c6e83e5b44d788420039c
ostap <ostapstephan@gmail.com>
2018-10-02 00:26:26 -0400

    65-67% but way overfitted

--- hw4curro/CIFAR10.py
+++ hw4curro/CIFAR10.py
@@ -15,7 +15,7 @@ from keras import regularizers
 #from keras import backend as K
 
 num_classes=10
-BATCH_SIZE = 256
+BATCH_SIZE = 512
 epochs = 24 #we achieve overfitting after like 15-20 epochs
 DROP_RATE =.4
 
@@ -28,7 +28,7 @@ def genTrainAndVal(f,l): #split the features and labels of the training data 80:
 	fs = f[s]			# features shuffled
 	ls = l[s]			# labels shuffled 
 	lx = f.shape[0] 	# len of the features
-	nv = int( lx *.2) 	# num validation samp 
+	nv = int( lx *.1) 	# num validation samp 
 	print (fs[nv:].shape, ls[nv:].shape, fs[:nv].shape, ls[:nv].shape)
 	return fs[nv:], ls[nv:], fs[:nv], ls[:nv]
 
@@ -59,35 +59,49 @@ y_v = keras.utils.to_categorical(y_v,num_classes)
 y_test  = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
-model.add(Conv2D(128,kernel_size=(2,2),
+model.add(Conv2D(64,kernel_size=(5,5),
+	strides= 1,
+	padding='valid',
+	activation='relu',
+	data_format='channels_last',
+	input_shape=x_t[0].shape))
+
+model.add(MaxPooling2D(pool_size=(2,2),
+	strides=2))
+
+model.add(Conv2D(256,kernel_size=(1,1),
+	strides= 1,
+	activation='relu',
+	padding='valid',
+	data_format='channels_last',
+	kernel_initializer='glorot_uniform',
+	input_shape=x_t[0].shape)
+	)
+
+model.add(Conv2D(1024,kernel_size=(2,2),
 	strides= 2,
 	padding='valid',
 	activation='relu',
 	data_format='channels_last',
 	kernel_initializer='glorot_uniform',
 	input_shape=x_t[0].shape))
-model.add(MaxPooling2D(pool_size=(2,2),
-	strides=2))
 
-
-model.add(Conv2D(256,kernel_size=(4,4),
-	strides= 4,
-	padding='valid',
+model.add(Conv2D(2048,kernel_size=(1,1),
+	strides= 1,
 	activation='relu',
 	data_format='channels_last',
 	kernel_initializer='glorot_uniform',
 	input_shape=x_t[0].shape))
-model.add(MaxPooling2D(pool_size=(2,2),
-	strides=2))
 
-model.add(Dropout(DROP_RATE))
+
+
 model.add(Flatten())
-model.add(Dense(2048,activation='relu'))
+model.add(Dense(256,activation='relu'))
 model.add(Dropout(DROP_RATE))
 model.add(Dense(num_classes,activation="softmax"))
 
-model.compile(loss=keras.losses.categorical_crossentropy,
-	optimizer=keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False),
+model.compile(loss=keras.losses.categorical_crossentropy ,
+	optimizer=keras.optimizers.Adam(lr=0.001),
 	metrics=['accuracy'])
 
 model.fit(x_t, y_t,
@@ -99,40 +113,5 @@ model.fit(x_t, y_t,
 score = model.evaluate(x_test,y_test,verbose= 1)
 print("Test loss:",score[0])
 print("Test accuracy:",score[1])
-
-
-'''
-def model():
-	model = Sequential()
-	
-	model.add(Dense(units=64, activation='relu', input_dim=100))
-	model.add(Dense(units=10, activation='softmax'))
-
-	model.compile(loss='categorical_crossentropy',
-		optimizer='sgd',
-		metrics=['accuracy'])
-
-	model.compile(loss=keras.losses.categorical_crossentropy,
-		optimizer=keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True))
-
-def genTrainAndVal(f,l): #split the features and labels of the training data 80:20 train and validation
-	valPercent=20	
-	lx,_,_=f.shape
-	z = f.shape[0]		# 60000 hopefully
-	s = np.arange(z)
-	np.random.shuffle(s)
-	fs = f[s]			# features shuffled
-	ls = l[s]			# labels shuffled 
-	lx = f.shape[0] 	# len of the features
-	nv = int( lx *.2) 	# num validation samp 
-	print (fs[nv:].shape, ls[nv:].shape, fs[:nv].shape, ls[:nv].shape)
-	return fs[nv:], ls[nv:], fs[:nv], ls[:nv]
-
-#send this the output of only the first 2 returned val of genTrainAndVal 
-def getBatch(feat,lab):
-	l,_,_= feat.shape
-	choices = np.random.choice(l, size=BATCH_SIZE)
-	return feat[choices],lab[choices]
-'''
 
 
