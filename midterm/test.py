d4e96e87aeda06454d7979f73144462d5e0d0cd5
ostap <ostapstephan@gmail.com>
2018-10-21 16:55:46 -0400

    having issues witht the envs

--- midterm/test.py
+++ midterm/test.py
@@ -2,14 +2,14 @@ import gym
 import tensorflow
 
 
-env = gym.make('Humanoid-v2')
+
+env = gym.make('BipedalWalker-v2')
 
 for i in range(20):
 	observation = env.reset()
 	for t in range(100):
 		env.render()
-		print(observation)
-		print(env.action_space)
+
 		action = 0#env.action_space.sample()
 		print(env.observation_space)
 		observation, reward, done, info = env.step(action)
@@ -17,4 +17,5 @@ for i in range(20):
 		if done:
 			print("Episode finished after {} timesteps".format(t+1))
 			break
+
 		'''
