e9fac2325345ce74cc370257ed82548e8f47b373
ostap <ostapstephan@gmail.com>
2018-10-21 20:28:53 -0400

    finally got the env working for qwop

--- midterm/test.py
+++ midterm/test.py
@@ -1,21 +1,11 @@
 import gym 
 import tensorflow
-
+#import box2d
 
 
 env = gym.make('BipedalWalker-v2')
 
-for i in range(20):
-	observation = env.reset()
-	for t in range(100):
-		env.render()
-
-		action = 0#env.action_space.sample()
-		print(env.observation_space)
-		observation, reward, done, info = env.step(action)
-		'''
-		if done:
-			print("Episode finished after {} timesteps".format(t+1))
-			break
-
-		'''
+env.reset()
+for _ in range(1000):
+    env.render()
+    env.step(env.action_space.sample())
